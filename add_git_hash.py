75681724e433e6b3e7f3dbf352f85368a1bfb787
Derrick K. DeConti <deconti.d@gmail.com>
2019-07-30 16:36:43 -0400

    Added git info python handlers.

---
+++ add_git_hash.py
@@ -0,0 +1,11 @@
+def add_to_context(request, workflow_obj, context_dict, context_args):
+    '''
+    Here we add the git repository URL to the context.  This lets us
+    put the repository url into reports, etc.
+
+    Here we are adding a hidden input.  The context_args dict has a key named
+    "variable_name", which gives us the name of the variable to fill-in in the HTML
+    template when the view is requested
+    '''
+    var_name = context_args['variable_name']
+    context_dict[var_name] = workflow_obj.git_commit_hash
