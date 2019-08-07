757fbc6ddec3756975242f04ea8ef26ca2cb5fd9
Derrick DeConti <ded084@Derricks-MacBook-Pro.local>
2019-08-06 22:10:45 -0400

    Added genome mapping w/o final paths.

---
+++ genome_mapper.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+def map_inputs(user, all_data, data_name, id_list):
+    '''
+    This maps the genome string to the resources needed to run a WDL
+    This is important because Cromwell localizes ALL files, so providing
+    a Map[String, File] will pull all the files listed therein.
+
+    unmapped_data is a string giving the genome
+    id_list is a list of the WDL input names.  The order is given in the gui.json
+    file.
+    '''
+
+    genome_choice = all_data[data_name]
+    this_directory = os.path.dirname(os.path.abspath(__file__))
+    resource_file = os.path.join(this_directory, 'genome_resources.json')
+    j = json.load(open(resource_file))
+
+    d = {}
+    d[id_list[0]] = genome_choice
+    d[id_list[1]] = j[genome_choice]['cellranger_reference']
+    d[id_list[2]] = j[genome_choice]['scmatch_reference']
+    return d
